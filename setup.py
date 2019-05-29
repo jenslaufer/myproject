3b4fc1604eef90497f31b75161891f31112103da
Jens Laufer <jenslaufer@gmail.com>
2019-05-29 08:13:58 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='mymodule',
+      version='0.1.0',
+      author='Jens Laufer',
+      author_email='jenslaufer@jenslaufer.com',
+      install_requires=[],
+      packages=find_packages()
+      )
